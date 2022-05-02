81d984438244206490620c41c6360003ecc93f01
Juan Pablo Velez <juan.velez993@gmail.com>
2022-05-02 17:38:36 -0500

    created the proyect and added note entity

---
+++ src/main/java/com/sofkau/todolist/entity/Note.java
@@ -0,0 +1,23 @@
+package com.sofkau.todolist.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity(name = "Note")
+@Table(name = "note")
+@Data
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String message;
+
+    private Boolean done;
+
+
+}
