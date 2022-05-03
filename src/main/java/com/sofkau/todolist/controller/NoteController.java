75c60d988f46a2296efcd3f8173ec6a67b617379
Juan Pablo Velez <juan.velez993@gmail.com>
2022-05-02 20:59:07 -0500

    ADD---controller for note entity

--- src/main/java/com/sofkau/todolist/controller/NoteController.java
+++ src/main/java/com/sofkau/todolist/controller/NoteController.java
@@ -1,4 +1,38 @@
 package com.sofkau.todolist.controller;
 
+import com.sofkau.todolist.entity.Note;
+import com.sofkau.todolist.service.NoteServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/")
+@CrossOrigin(origins="http://localhost:3000/")
 public class NoteController {
+
+    @Autowired
+    private NoteServiceImplementation service;
+
+    @GetMapping("get/notes")
+    public List<Note> getNotes(){
+        return service.getNotes();
+    }
+
+    @PostMapping("save/note")
+    public Note saveNote(@RequestBody Note note){
+        return service.saveNote(note);
+    };
+
+    @PutMapping("update/note")
+    public Note updateNote(@RequestBody Note note){
+        return service.updateNote(note);
+    };
+
+
+    @DeleteMapping("delete/note/{id}")
+    public void deleteNote(@PathVariable Long id){
+        service.deleteNote(id);
+    };
 }
